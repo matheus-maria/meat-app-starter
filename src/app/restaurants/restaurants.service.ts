83befa2114d3f84e95c8427a16b2f8c5fcaed93f
Matheus Maria <matheus.maria@oransys.net>
2019-01-03 09:39:41 -0200

    Error Handler

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,6 +4,8 @@ import { Http } from "@angular/http";
 import { MEAT_API } from "app/app.api";
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { ErrorHandler } from "app/app-error-handler";
 
 @Injectable()
 export class RestaurantService {
@@ -15,5 +17,6 @@ export class RestaurantService {
     restaurants = (): Observable<Restaurant[]> => {
         return this.http.get(`${MEAT_API}/restaurants`)
         .map(response => response.json())
+        .catch(ErrorHandler.handleError)
     }
 }
