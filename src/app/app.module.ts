2618b941cf2717262d6ad5c265d5c5f6723f8c69
Matheus Maria <mmatheus.maria@gmail.com>
2019-01-06 18:38:29 -0200

    PreLoading with Lazy Loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -35,7 +35,7 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule.forRoot(ROUTES),    
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules}),    
     SharedModule,
     CoreModule
   ],
